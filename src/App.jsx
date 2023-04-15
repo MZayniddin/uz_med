2b41ec58dae0866bd674a8398a63100ee2eccb59
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-15 12:04:41 +0500

    header started

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "./layouts/header/Header";
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <Outlet />
         </div>
     );
