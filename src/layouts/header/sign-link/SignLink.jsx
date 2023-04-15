e0570a3c0b33af691e81776eb6680647fe45e20f
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-15 15:45:37 +0500

    opened sign link

---
+++ src/layouts/header/sign-link/SignLink.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const SignBtn = () => {
+    return (
+        <Link className="header__sign-link" to="#">
+            Sign In
+        </Link>
+    );
+};
+
+export default SignBtn;
