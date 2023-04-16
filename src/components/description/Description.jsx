769ba7e3df37a9d55bad9a8c058b7edeff02c3fc
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-16 16:14:21 +0500

    description component was finished

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,7 +1,34 @@
+import { v4 as uuidv4 } from "uuid";
 import "./Description.scss";
 
-export default function Description() {
-    return <section className="desc">
-        <h2 className="desc__title"></h2>
-    </section>;
+export default function Description({
+    data: { title, description, duration, lead, objectives },
+}) {
+    return (
+        <section className="desc">
+            <div className="container">
+                <h2 className="desc__title section-title">{title}.</h2>
+                <div className="desc__sub-title">Description:</div>
+                <p className="desc__info desc__text">{description}</p>
+                <div className="desc__text">
+                    <strong>Duration: </strong>
+                    {duration}
+                </div>
+                <div className="desc__text">
+                    <strong>Lead Beneficiary: </strong>
+                    {lead}
+                </div>
+                <div className="desc__text">
+                    <strong>Objectives: </strong>
+                </div>
+                <ol className="desc__list">
+                    {objectives.map((obj, idx) => (
+                        <li className="desc__text" key={uuidv4()}>
+                            {idx + 1}. {obj}
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        </section>
+    );
 }
--- src/pages/Management.jsx
+++ src/pages/Management.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+
+// COMPONENTS
 import Description from "../components/description/Description";
+
+// DATA
+import data from "../data/management.json";
 
 const Management = () => {
     return (
         <main>
-            <Description />
+            <Description data={data[0].data_page} />
         </main>
     );
 };
