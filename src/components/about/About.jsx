3815efc73e96ed55c244e301f79161df40bb30dc
01werka <toshpolatovsherzod1@gmail.com>
2023-04-16 15:47:23 +0500

    added about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,73 @@ import "./About.scss";
 const About = () => {
     return (
         <section className="about">
-            <div className="container">About</div>
+            <div className="container">
+                <h1 className="about_h1">ABOUT THE PROJECT.</h1>
+                <div className="line_about"></div>
+                <div className="about_title">
+                    <h1 className="about_title_h1">Title of the project:</h1>
+                    <div className="line_title"></div>
+                    <p className="about_title_text">Modernization of Mechatronics and Robotics for Bachelor's degree in Uzbekistan through Innovative Ideas and Digital Technology</p>
+
+                </div>
+                <div className="about_main">
+                    <h1 className="about_main_h1">Main project outcomes:</h1>
+                    <div className="line_main"></div>
+
+                    <p className="about_main_text">1. Developing standards and curricula, courses, teaching methods and materials;</p>
+                    <p className="about_main_text">2. Developing and implementing new four training courses;</p>
+                    <p className="about_main_text">3. Developing and publishing a new generation of handbooks; </p>
+                    <p className="about_main_text">4. Establishing the laboratories, Regional Training Centers and Medical engineers Society;</p>
+                    <p className="about_main_text">5. Testing, adapting and accrediting curriculum, materials and methods;</p>
+                    <p className="about_main_text">6. The involvement of females, migrants and people suffer from social and economic barriers to the training courses and master's programs </p>
+                </div>
+
+                <div className="about_consortium">
+                    <h1 className="about_consortium_h1">Consortium:</h1>
+                    <div className="line_consortium"></div>
+                    <p className="about_consortium_text"><b>Coordinator: </b>Andijan machine-building institute <b>(Andijan, Uzbekistan)</b></p>
+                    <p className="about_consortium_text"><b>European Partners:</b>  Rigas Tehniska Universitate <b> (Riga, Latvia),</b>  Vilnius Gediminas Technical University <b>(Vilnius, Lithuania).</b></p>
+                    <p className="about_consortium_text"><b>Partners from Uzbekistan:</b> Andijan machine-building institute <b>(Andijan, Uzbekistan),</b>Turin Polytechnic University in Tashkent <b>(Tashkent, </b></p>
+                    <p className="about_consortium_text"><b>  Uzbekistan),</b>   Kimyo International University in Tashkent <b> (Tashkent,  Uzbekistan)</b> Tashkent University of Information Technology <b> (Tashkent,</b> </p>
+                    <p className="about_consortium_text"><b> Uzbekistan),</b> Bukhara State Medical Institute <b>(Buhkara, Uzbekistan),</b> Bukhara Engineering Technological Institute <b>( Buhkara,Uzbekistan),</b> </p>
+                    <p className="about_consortium_text">  Ministry of Higher and Secondary Specialised Education System of Uzbekistan <b>(Tashkent, Uzbekistan),</b>  Center of development of medical</p>
+                    <p className="about_consortium_text"> education Ministry of public health of the republic of Uzbekistan <b>(Tashkent, Uzbekistan)</b></p>
+                    <p className="about_consortium_text"><b>Associated Partners:</b>Tibbiyot texnikalariga xizmat ko'rsatish o'quv ishlab chiqarish markazi <b> (Tashkent, Uzbekistan),</b> "MAKRO FARM ANDIJAN" Ltd </p>
+                    <p className="about_consortium_text"><b>(Asaka, Uzbekistan)</b></p>
+
+                </div>
+
+                <div className="about_duration">
+                    <h1 className="about_duration_h1">Duration of the project:</h1>  
+                    <p className="about_duration_text"> 01/02/2023 – 01/12/2026 36 Months</p>
+                </div>
+                <div className="line_duration"></div>
+               
+                <div className="about_duration">
+                    <h1 className="about_duration_h1">Action specific activities:</h1>  
+                    <p className="about_duration_text"> Curriculum Development</p>
+                </div>
+                <div className="line_duration_2"></div>
+
+                <div className="about_duration">
+                    <h1 className="about_duration_h1">Type of project:</h1>  
+                    <p className="about_duration_text"> National Project</p>
+                </div>
+                <div className="line_duration_3"></div>
+
+                <div className="about_duration">
+                    <h1 className="about_duration_h1">Submission number:</h1>  
+                    <p className="about_duration_text"> ERASMUS-EDU-2022-CBHE-STRAND-2</p>
+                </div>
+                <div className="line_duration_4"></div>
+
+                <div className="about_duration">
+                    <h1 className="about_duration_h1">Budget:</h1>  
+                    <p className="about_duration_text"> 798 695 euros</p>
+                </div>
+                <div className="line_duration_5"></div>
+            </div>
+
         </section>
     );
 };
--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,9 +1,24 @@
 import "./News.scss";
+import section from "../../assets/images/section-bg.jpg"
 
 const News = () => {
     return (
         <section>
-            <div className="container">News</div>
+            <div className="container">
+
+                <h1 className="about_newsh1">NEWS</h1>
+                <div className="line_news_1"></div>
+                <div className="new_box">
+                    <img className="new_box_img" src={section} alt="" />
+                    <div className="new_box_text">
+                        <h1 className="new_box_text1">Training and research centers (labs) in the  framework of the ERASMUS PLUS projects at TTPU</h1>
+                        <p className="new_box_text2">Training and research centers (labs) in the  framework of th...</p>
+                    </div>
+                </div>
+
+
+            </div>
+
         </section>
     );
 };
