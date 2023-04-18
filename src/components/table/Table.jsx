2beaa6c0b1c2d4dda39cd32f97bf96241f2b64b2
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-18 23:25:59 +0500

    table component was finished

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,6 +1,13 @@
+import { v4 as uuidv4 } from "uuid";
+
+// DATA
+import data from "../../data/management.json";
+
+// STYLE
 import "./Table.scss";
 
 const Table = () => {
+    console.log();
     return (
         <section className="table__wrapper">
             <div className="container">
@@ -17,7 +24,29 @@ const Table = () => {
                             <th>Role</th>
                         </tr>
                     </thead>
-                    <tbody></tbody>
+                    <tbody>
+                        {data[0].data_table.map(
+                            ({
+                                task_no,
+                                task_name,
+                                description,
+                                participant_name,
+                                participant_role,
+                            }) => (
+                                <tr key={uuidv4()}>
+                                    <td className="col_task_no">{task_no}</td>
+                                    <td>{task_name}</td>
+                                    <td className="col_desc">{description}</td>
+                                    <td className="col_participant">
+                                        {participant_name}
+                                    </td>
+                                    <td className="col_participant">
+                                        {participant_role}
+                                    </td>
+                                </tr>
+                            )
+                        )}
+                    </tbody>
                 </table>
             </div>
         </section>
