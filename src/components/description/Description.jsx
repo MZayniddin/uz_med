380ad2324c9d5c2f622fbb275db600e4209969a7
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-07-08 23:03:17 +0500

    changed description component's styles

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -2,33 +2,27 @@ import { v4 as uuidv4 } from "uuid";
 import "./Description.scss";
 
 export default function Description({
-    data: { title, description, duration, lead, objectives },
+  data: { title, description, duration, lead, objectives },
 }) {
-    return (
-        <section className="desc">
-            <div className="container">
-                <h2 className="desc__title section-title">{title}.</h2>
-                <div className="desc__sub-title">Description:</div>
-                <p className="desc__info desc__text">{description}</p>
-                <div className="desc__text">
-                    <strong>Duration: </strong>
-                    {duration}
-                </div>
-                <div className="desc__text">
-                    <strong>Lead Beneficiary: </strong>
-                    {lead}
-                </div>
-                <div className="desc__text">
-                    <strong>Objectives: </strong>
-                </div>
-                <ol className="desc__list">
-                    {objectives.map((obj, idx) => (
-                        <li className="desc__text" key={uuidv4()}>
-                            {idx + 1}. {obj}
-                        </li>
-                    ))}
-                </ol>
-            </div>
-        </section>
-    );
+  return (
+    <section className="desc">
+      <div className="container">
+        <h2 className="desc__title section-title">{title}</h2>
+        <h4 className="desc__sub-title">Description</h4>
+        <p className="desc__text text">{description}</p>
+        <h4 className="desc__sub-title">Objectives</h4>
+        <ol className="desc__list desc__info">
+          {objectives.map((obj, idx) => (
+            <li className="text" key={uuidv4()}>
+              {idx + 1}. {obj}
+            </li>
+          ))}
+        </ol>
+        <h4 className="desc__sub-title">Lead Beneficiary</h4>
+        <p className="text desc__info">{lead}</p>
+        <h4 className="desc__sub-title">Duration</h4>
+        <p className="text desc__info">{duration}</p>
+      </div>
+    </section>
+  );
 }
